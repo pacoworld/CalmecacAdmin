ba21b577e1de812306cd73ad7b8c423825965031
Francisco Sanchez <pacoworld@gmail.com>
2020-08-27 13:00:05 -0500

    Addging form Primerusuario to register first user

---
+++ FormaAplicacion/PrimerUsuario.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FormaAplicacion
+{
+    public partial class PrimerUsuario : Form
+    {
+        SqlConnection cs = new SqlConnection("Data Source = .\\sqlexpress; Initial Catalog = DatabasePaco; Integrated Security = TRUE");
+        SqlDataAdapter da = new SqlDataAdapter();
+
+        public PrimerUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text != textBox5.Text)
+            {
+                MessageBox.Show("La clave no coincide con la confirmación");
+            }
+            else
+            {
+                da.InsertCommand = new SqlCommand("INSERT INTO USUARIOS VALUES (@LOGIN, @NOMBRE, @APELLIDO, @CLAVE)", cs);
+                da.InsertCommand.Parameters.Add("@LOGIN", SqlDbType.VarChar).Value = textBox1.Text;
+                da.InsertCommand.Parameters.Add("@NOMBRE", SqlDbType.VarChar).Value = textBox2.Text;
+                da.InsertCommand.Parameters.Add("@APELLIDO", SqlDbType.VarChar).Value = textBox3.Text;
+                da.InsertCommand.Parameters.Add("@CLAVE", SqlDbType.VarChar).Value = textBox4.Text;
+                cs.Open();
+                da.InsertCommand.ExecuteNonQuery();
+                cs.Close();
+                MessageBox.Show("El Usuario " + textBox1.Text + "ha sido agregado");
+                this.Hide();
+                Password pas = new Password();
+                pas.Show();
+            }
+        }
+    }
+}
--- FormaAplicacion/Program.cs
+++ FormaAplicacion/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace FormaAplicacion
 {
@@ -11,13 +12,43 @@ namespace FormaAplicacion
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
+        
         [STAThread]
         static void Main()
         {
-            
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Password());
+            SqlConnection cs = new SqlConnection("Data Source = .\\sqlexpress; Initial Catalog = DatabasePaco; Integrated Security = TRUE");
+            SqlCommand com1;
+            SqlDataReader reader1;
+            object temp1;
+            int temp = 0;
+
+            cs.Open();
+            string QuertyTotal = "select count (*) from usuarios ";
+            com1 = new SqlCommand(QuertyTotal, cs);
+            reader1 = com1.ExecuteReader();
+
+            if (reader1.Read())
+            {
+                temp1 = reader1[0];
+                temp = Convert.ToInt32(temp1);
+            }
+            cs.Close();
+
+            if (temp == 0)
+            {
+                MessageBox.Show("No hay usuarios, tienes que registrar uno");
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new PrimerUsuario());
+
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Password());
+            }
             
         }
     }
